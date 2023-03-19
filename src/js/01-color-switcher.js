5018c8cae5451873892dbb2eefe2612e53a90b6f
roman10rb <roma10rb@gmail.com>
2023-03-19 20:31:52 +0200

    first task

---
+++ src/js/01-color-switcher.js
@@ -0,0 +1,42 @@
+
+const refs = {
+    start: document.querySelector("[data-start]"),
+    stop: document.querySelector("[data-stop]"),
+    body: document.body,
+};
+
+let olreadyStart;
+refs.start.addEventListener('click', () => {
+    if (olreadyStart) {
+        return;
+    };
+    colorInterval = setInterval(() => {
+        olreadyStart = true;
+        refs.body.style.backgroundColor = getRandomHexColor();
+    }, 1000);
+    
+});
+
+refs.stop.addEventListener('click', () => {
+    clearInterval(colorInterval);
+    olreadyStart = false;
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+}
