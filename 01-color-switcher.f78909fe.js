e1d3ceda87aa4d168442e5070e923cc0ea6ead1b
roman10rb <114180387+roman10rb@users.noreply.github.com>
2023-03-21 06:25:48 +0000

    Deploying to gh-pages from @ roman10rb/goit-js-hw-09@7f8b1ccf1351a03d0d452a588a5ee931e27b53c8 🚀

--- 01-color-switcher.f78909fe.js
+++
@@ -1,2 +0,0 @@
-const t={start:document.querySelector("[data-start]"),stop:document.querySelector("[data-stop]"),body:document.body};let e=null;t.start.addEventListener("click",(()=>{e||(colorInterval=setInterval((()=>{e=!0,t.body.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,0)}`}),1e3))})),t.stop.addEventListener("click",(()=>{clearInterval(colorInterval),e=!1}));
-//# sourceMappingURL=01-color-switcher.f78909fe.js.map
